c091f508041416f0ab5349223951d2b8f4b3c14f
Dhoka, Nikhil <ndhoka@iu.edu>
2024-09-02 14:00:17 -0400

    Added API

--- src/Package.ts
+++ src/Package.ts
@@ -6,19 +6,21 @@ import { ResponsiveMaintainer } from "./ResponsiveMaintainer";
 export class Package {
     public url: string = "";
     public version: string = "";
-    private metrics: AllMetrics = new AllMetrics(this.url, this.version);
+    private packageMetrics: AllMetrics;
 
     constructor(url: string, version: string) {
         this.url = url;
         this.version = version;
+        this.packageMetrics = new AllMetrics(url, version);
     }
 
     public getMetrics(): any {
         // return a json object of all the values of the metrics
+        console.log("Getting Metrics");
         return {
             url: this.url,
-            NetScore: this.metrics.getNetScore(),
-            NetScoreLatency: this.metrics.getNetScoreLatency()
+            NetScore: this.packageMetrics.getNetScore(),
+            NetScoreLatency: this.packageMetrics.getNetScoreLatency()
             //add other metrics
         };
     }
