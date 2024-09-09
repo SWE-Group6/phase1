7f8f12fadb9ccb76dc263e111744fa4ab21c9798
celedoniogarcia <cg3122492@gmail.com>
2024-09-08 22:17:33 -0400

    updating code to account for getting both readme and license files

--- src/Models/License.ts
+++ src/Models/License.ts
@@ -4,6 +4,7 @@
  * their license file using GitHub API. As stated on our project board:
  * The license of the npm package should be compatible with the GNU Lesser General Public License v2.1. 
  * If the license is fully compatible, the package should score higher.
+ * Proper documentation and accessibility of the license is crucial. Points could be awarded based on whether the license is clearly stated:
 */
 
 import {Metric} from "./Metric";
@@ -17,21 +18,23 @@ import * as https from 'https';
 
 export class License extends Metric {
     // arbitrary weight given to this metric.
-    public weight: number = 0.2;
+    private weight: number = 0.2;
 
     constructor(url: string, version: string) {
         super(url, version);
     }
     
+    // update: renamed to getRepoFile due to this method getting called when
+    // retrieving both LICENSE and README files.
     // PURPOSE: fetch a repo's license using built in GET method.
     // EXPECTED OUTPUT: return an object containing the content of the license.
     // PARAMTERS: owner: string, repo: string
-    getLicenseFile(owner: string, repo: string, path: string): Promise<any> {
+    getRepoFile(owner: string, repo: string, path: string): Promise<any> {
         // configuration needed to access the GitHub API.
         // found through: https://docs.github.com/en/rest/using-the-rest-api/getting-started-with-the-rest-api?apiVersion=2022-11-28#http-method
         const options = {
             hostname: 'api.github.com',
-            path: `/repos/${owner}/${repo}/${path}`, // go straight to the license.
+            path: `/repos/${owner}/${repo}/${path}`, // path variable set to get whatever file is needed.
             method: 'GET',
             headers: {
                 'User-Agent': 'node.js', // GitHub requires this for requests. Without it, request rejected automatically!
@@ -76,29 +79,36 @@ export class License extends Metric {
         });
     }
 
-    async fetchLicense(owner: string, repo: string, path: string): Promise<string | null> {
+    // update: renamed to fetchFileContent for same reasons as getFile and it
+    // will return the decoded file content.
+    // PURPOSE: decode a file gotten from a GET request.
+    // EXPECTED OUTPUT: content of file: string or null.
+    // PARAMETERS: owner: string, repo: string, path: string.
+    async decodeContent(owner: string, repo: string, path: string): Promise<string | null> {
         try {
             // call the method to get the file
-            const licenseData = await this.getLicenseFile(owner, repo, path);
+            const fileData = await this.getRepoFile(owner, repo, path);
             // if the license was successfully fetched
-            if (licenseData && licenseData.content) {
-                const decodedLicense = Buffer.from(licenseData.content, 'base64').toString('utf-8');
-
-                const licenseType = this.identifyLicense(decodedLicense);
+            if (fileData && fileData.content) {
+                const decodedFile = Buffer.from(fileData.content, 'base64').toString('utf-8');
 
                 // return the license type regardless if it exists or not.
-                if (licenseType) {
-                    console.log(`License type is: ${licenseType}`);
-                    return licenseType; 
+                if (decodedFile) {
+                    return decodedFile;
                 } else {
-                    console.log('Could not identify the license type');
+                    console.log('Could not decode the file.');
                     return null;
                 }
             }
             return null;
+        // error documentation in typescript: https://engineering.udacity.com/handling-errors-like-a-pro-in-typescript-d7a314ad4991
         } catch (error) {
-            console.error('Error fetching info: ', error);
-            return null;
+            if (error instanceof Error) {
+                console.error(error.message);
+                return null;
+            } else {
+                console.error('An unknown error occured:', error);
+            }
         }
     }
 
@@ -123,6 +133,14 @@ export class License extends Metric {
         } else {
             return null;  // License type could not be determined
         }
+    }
+
+    // PURPOSE: rate a packages' license based on conditions discussed as a
+    // group.
+    // EXPECTED OUTPUT: number (float).
+    // PARAMTERS:
+    rateLicense(): number {
+
     }
 
     // PURPOSE: give a repo a score based on type of license they have.
