ffd41701e332e708f14a9afa6b043acb508d8590
celedoniogarcia <cg3122492@gmail.com>
2024-09-09 00:41:33 -0400

    started working on ramp up metric, most of it shares logic from license metric, which I wrote first

--- src/Models/RampUp.ts
+++ src/Models/RampUp.ts
@@ -21,28 +21,50 @@ export class RampUp extends Metric {
         super(url, version);
     }
 
-    // PURPOSE: fetch a repo's README file. 
+    // PURPOSE: fetch a repo's README file using built in GET method.
     // EXPECTED OUTPUT: return an object containing the content of the README
     // for GPT to analyze.
     // PARAMTERS: 1. owner of repo: string; 2. repo url: string
-    async getRepoFiles(owner: string, repo: string) {
-        // create a header to provide authentication to GitHub API.
-        // const headers = { Authorization: `Bearer ${GITHUB_TOKEN}` };
+    getRepoFile(owner: string, repo: string) {
         const options = {
-            hostname: 'api.github.com', 
-            path: `/repos/${owner}/${repo}/README`,
+            hostname: 'api.github.com',
+            path: `/repos/${owner}/${repo}/${path}`,
             method: 'GET',
-        }
+            headers: {
+                'User-Agent': 'node-js',
+                'Accept': 'application/vnd.github.v3+json',
+            }
+        };
+        
+        return new Promise((resolve, reject) => {
+            https.get(options, (response) => {
+                let data = '';
+
+                response.on('data', (chunk) => {
+                    data += chunk;
+                });
+                
+                response.on('end', () => {
+                    try {
+                        const parsedData = JSON.parse(data);
+
+                        if (response.statusCode !== 200) {
+                            reject(new Error(`Something went wrong while parsing the data: ${parsedData.message}`));
+                        } else {
+                            resolve(parsedData);
+                        }
+                    } catch (error) {
+                        if (error instanceof Error) {
+                            reject(new Error(`Couldn't parse the data: ${error.message}`));
+                        } else {
+                            console.error('An unknown error occured');
+                        }
+                    }
+                });
+            });
+        });
     }
 
-/*
-    // PURPOSE: analyze a README file using GPT API.
-    // EXPECTED OUTPUT: number
-    // PARAMETERS: 1. README
-    async analyzeRampUp(readmeContent: string) {
-       // TODO: flesh out function 
-    }
-*/
     calculateScore(url: string, version: string): void {
         console.log("Calculating RampUp");
         const start = performance.now();
