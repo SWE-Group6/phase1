b2a82b95702d8d89c4bce83fc230c8366cc78b25
celedoniogarcia <cg3122492@gmail.com>
2024-09-21 17:19:04 -0400

    constructor breaks url into components if github url

--- src/Models/License.ts
+++ src/Models/License.ts
@@ -1,10 +1,26 @@
+/* Celedonio G. 
+ * NAME: License Metric
+ * 
+*/
+
 import {Metric} from "./Metric";
 
 export class License extends Metric {
     public weight: number = 0.2;
+    private owner: string = '';
+    private repo: string = '';
 
     constructor(url: string) {
         super(url);
+
+        // if the url is GitHub, break down into components for later
+        // processing.
+        if (url.includes('github.com')) {
+            // generic github url is: https://github.com/owner/repo
+            const parts = url.split('/');
+            this.owner = parts[3];
+            this.repo = parts[4];
+        } 
     }
 
     calculateScoreGithub(): void {
